2404ab8c9ee6238b2c5032c072abb346aece85da
aarizsak <120405944+aarizsak@users.noreply.github.com>
2023-12-07 19:46:47 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,9 +5,15 @@ terraform {
       version = "~> 4.16"
     }
   }
-  required_version = ">= 1.2.0"
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region = var.aws_region
+  region     = "var.aws_region"
+  access_key = "AKIASJQYUW57ZGAXHE6F"
+  secret_key = "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"
 }
+
+
+
+
