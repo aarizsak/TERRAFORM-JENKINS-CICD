e67004fed453294c9dc561d5cddeabee03a62eb9
aarizsak <120405944+aarizsak@users.noreply.github.com>
2023-12-07 19:38:14 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "us-west-2"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = "jenkins"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,7 +12,7 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-093467ec28ae4fe03"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
